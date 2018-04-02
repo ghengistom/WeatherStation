6424070a5d765e3137bc6128dba8f97f60dbb642
Tom <blackbirdmach6@gmail.com>
2018-04-02 07:52:56 -0700

    w

--- NodeSensorScripts/databaseCalls.js
+++ NodeSensorScripts/databaseCalls.js
@@ -8,11 +8,11 @@ function insertToDB(){
 
     let db = new sqlite3.Database('/home/pi/TempSensor/weatherdatabase.db');
 
-    var temp = getTemp.getTempOnInterval();
+    var temp1 = getTemp.getTempOnInterval();
 
 
     // insert one row into the langs table
-    db.run(`INSERT INTO timetemp(time, temp) VALUES(?,?)`, [ date , temp ], function(err) {
+    db.run(`INSERT INTO timetemp(time, temp) VALUES(?,?)`, [ date , temp1 ], function(err) {
     if (err) {
         return console.log(err.message);
     }
