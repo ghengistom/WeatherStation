4c94c8e522e8a355fb7974e332993421bed444cd
ghengisotm <bernard5150@gmail.com>
2018-04-01 18:40:57 -0700

    checking which functions i need

--- NodeSensorScripts/ds18x20.js
+++ NodeSensorScripts/ds18x20.js
@@ -6,7 +6,7 @@ var sensorid;
 
 var myVar;
 
-function myFunction() {
+function getTempOnInterval() {
     setInterval(function(){
             
             sensor.get('28-000006e0e2ae', function (err, temp) {          
@@ -15,6 +15,7 @@ function myFunction() {
                {
                    console.log('The sensor id is:  =  ', sensorid);
                    console.log('The temperature is: ', temp, ' degree celcius.');
+                   return temp;
      
                }
                else{
@@ -22,22 +23,18 @@ function myFunction() {
                }
                
             })
-    }, 3000);
+    }, 60000);
     
 }
-
-
-    
+   
 
     sensor.loadDriver(function (err) {
-        if (err) console.log('something went wrong loading the driver:', err)
+        if (err) console.log('something went wrong loading the driver: ', err)
         else {
             console.log('driver is loaded');
-            myFunction();
+            getTempOnInterval();
         }
     });
-
-
 
 
     sensor.list(function (err, listOfDeviceIds) {
@@ -49,18 +46,10 @@ function myFunction() {
         sensor.get(sensorid, function (err, temp) {
             console.log('this is sensor.get inside sensor.list', temp)
         });
-
-
-
     });
 
-
-
-
-    sensor.getAll(function (err, tempObj) {
-        console.log('this is sensor.getAll', tempObj);
-        console.log('this is the error', err);
-    });
-
-
+    // sensor.getAll(function (err, tempObj) {
+    //     console.log('this is sensor.getAll', tempObj);
+    //     console.log('this is the error', err);
+    // });
 
