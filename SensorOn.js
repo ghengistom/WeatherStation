d9aedf6263a4eebb70e3d3bb667a88b1cb7f8fa2
Tom Bernard <bernard5150@csu.fullerton.edu>
2018-04-03 18:30:00 +0000

    troubleshooting python script, it's not t repearting on interval.

--- SensorOn.js
+++ SensorOn.js
@@ -1,15 +1,24 @@
-var ids2;
+var ids2=28-000006e0e2ae;
 
 var ds18b20 = require('ds18b20');
 ds18b20.sensors(function(err, ids) {
 	  console.log('This is the error msg ' + err);
 
 	  console.log('\nThis is the sensor ids' + ids);
-ids2 = ids;
+
 });
 
-
+console.log('above temperature function');
 ds18b20.temperature(ids2, function(err, value){
 	console.log('Current temperature is', value);
 
 });
+
+
+ds18b20.temperature(ids2, {parser: 'hex'}, function(err, value) {
+  console.log('Current temperature is', value);
+
+});
+
+
+console.log('Current temperature is' + ds18b20.temperatureSync(ids2, {parser: 'hex'}));
