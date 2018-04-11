b0e4554c0194bd7e428d6e28298d1811a867e4e5
Tom <blackbirdmach6@gmail.com>
2018-04-11 09:14:43 -0700

    adding error handling for sensor.get

--- NodeSensorScripts/ds18x20.js
+++ NodeSensorScripts/ds18x20.js
@@ -1,57 +1,63 @@
-//const dbCall = require('./databaseCalls.js');
 var sensor = require('ds18x20');
 var sensorid;
 
 
 var myVar;
 
+
+
+sensor.loadDriver(function (err) {
+    if (err) console.log('something went wrong loading the driver: ', err)
+    else {
+        console.log('driver is loaded');
+        getTempOnInterval();
+    }
+});
+
+
+
+sensor.isDriverLoaded(function (err, isLoaded) {
+    console.log('this is sensor.isDriverLoaded', isLoaded);
+});
+
+
+
 function getTempOnInterval() {
     setInterval(function(){
-//<<<<<<< HEAD
             
                sensor.get('28-000006e0e2ae', function (err, temp) {
+                   if(!err){
+                      console.log('The sensor id is:  =  ', sensorid);
+                      console.log('The temperature is: ', temp, ' degree celcius.');
+                      return temp;
+                   }
+                   else{
+                       console.log('The error for sensor.get is: ', err);
+                   }
       
-               console.log('in sensor.get with sensorid = ', sensorid);
-               console.log('this is sensor.get', temp);
-               console.log('this is the error', err);
-            })
+              
+            }, 6000);
     }, 3000);
     
 }
 
 
-            sensor.get('28-000006e0e2ae', function (err, temp) {
-               if(!err){
-                   console.log('The sensor id is:  =  ', sensorid);
-                   console.log('The temperature is: ', temp, ' degree celcius.');
-                  // dbCall.insertToDB();
-                   return temp;
+            // sensor.get('28-000006e0e2ae', function (err, temp) {
+            //    if(!err){
+            //        console.log('The sensor id is:  =  ', sensorid);
+            //        console.log('The temperature is: ', temp, ' degree celcius.');
+            //       // dbCall.insertToDB();
+            //        return temp;
 
-               }
-               else{
-                   console.log('Error for sensor.get is : ', err);
-               }
+            //    }
+            //    else{
+            //        console.log('Error for sensor.get is : ', err);
+            //    }
 
-            }, 6000);
-    //}, 60000);
-
+            // }, 6000);
 
 
-
-//>>>>>>> d9aedf6263a4eebb70e3d3bb667a88b1cb7f8fa2
-    sensor.loadDriver(function (err) {
-        if (err) console.log('something went wrong loading the driver: ', err)
-        else {
-            console.log('driver is loaded');
-            getTempOnInterval();
-        }
-    });
-
-
-//<<<<<<< HEAD
-    sensor.isDriverLoaded(function (err, isLoaded) {
-        console.log('this is sensor.isDriverLoaded', isLoaded);
-    });
+  
 
 //=======
 /*Insert one row into a table */
@@ -73,4 +79,4 @@ function getTempOnInterval() {
     //     console.log('this is sensor.getAll', tempObj);
     //     console.log('this is the error', err);
     // });
-//>>>>>>> d9aedf6263a4eebb70e3d3bb667a88b1cb7f8fa2
+
