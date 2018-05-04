88de4993b4dd55225c3f5a0d037682fc1f7479d4
ERIKA CHUMPITAZI <CHUMP@ERIKAs-MBP.fios-router.home>
2018-05-03 23:09:03 -0700

    working on controller

---
+++ weatherWebServer.py
@@ -0,0 +1,13 @@
+from flask import Flask, request #import main Flask class and request object
+
+app = Flask(__name__) #create the Flask app
+
+
+@app.route('/gethistory', methods=['GET']) #GET requests will be blocked
+def get_history():
+    return 'Todo...'   #need to call getHistoryModel.py then return json 
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000) #run app in debug mode on port 5000
