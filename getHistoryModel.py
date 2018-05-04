547e42101573d7b3a3a97e6502a04f8e54b67200
ERIKA CHUMPITAZI <CHUMP@ERIKAs-MBP.fios-router.home>
2018-05-03 21:38:47 -0700

    syntax issues

--- getHistoryModel.py
+++ getHistoryModel.py
@@ -3,7 +3,11 @@ conn = sqlite3.connect('weatherdatabase.db')
 c = conn.cursor()
 
 
-c.execute('SELECT * FROM timetemp)
+c.execute(""" select * from timetemp """)
+data=c.fetchall()
+print(data)
+
+#c.execute('SELECT * FROM timetemp)
 #print(c.fetchall())   #print '"' + my_str + '"'
 #print('"""' + mylist + '"""')
 
