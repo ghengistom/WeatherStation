e32313b785ff771be13dcc9d1e6cae1e73a370df
ERIKA CHUMPITAZI <CHUMP@ERIKAs-MBP.fios-router.home>
2018-05-07 20:18:30 -0700

    jsonify

--- weatherWebServer.py
+++ weatherWebServer.py
@@ -1,4 +1,5 @@
 import getHistoryModel
+from flask import jsonify
 from flask import Flask, request #import main Flask class and request object
 
 app = Flask(__name__) #create the Flask app
@@ -6,15 +7,13 @@ app = Flask(__name__) #create the Flask app
 
 @app.route('/gethistory', methods=['GET']) #GET requests will be blocked
 def get_history():
-    #return 'Todo...'   #need to call getHistoryModel.py then return json 
-    return getHistoryModel.getHistory()
+    h = getHistoryModel.getHistory()
+    return jsonify(d)
 
 
 @app.route('/test1', methods=['GET']) #GET requests will be blocked
 def test1():
     return 'Todo...'   #need to call getHistoryModel.py then return json 
-
-
 
 
 
