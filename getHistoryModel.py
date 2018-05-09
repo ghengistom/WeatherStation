71326dcd37fd6d0e150f2063209c9c335c422e67
ERIKA CHUMPITAZI <CHUMP@ERIKAs-MBP.fios-router.home>
2018-05-08 19:22:55 -0700

    remove print in gethistory

--- getHistoryModel.py
+++ getHistoryModel.py
@@ -10,21 +10,11 @@ def getHistory():
   data=c.fetchall()
 
   toJson = json.dumps(data)
-  print(toJson)
+  #print(toJson)
 
   return toJson
 
-# for i,j in data:
-#     print(i)
-#     print(j)
 
-# for i in toJson:
-#     print(i)
-  
 
-#print(data)
 
-#c.execute('SELECT * FROM timetemp)
-#print(c.fetchall())   #print '"' + my_str + '"'
-#print('"""' + mylist + '"""')
 
