43bda0fef766b1fb5e97e438f9d2240395612857
ghengistom <tomedwardbernard@gmail.com>
2018-05-21 15:01:37 -0700

    testing json dumps format

--- gettemp.py
+++ gettemp.py
@@ -48,11 +48,11 @@ def gettemp_():
         #print('This is the temp is all units' + str(temperature_in_all_units))
 
             tojson = json.dumps(temperature_in_fahrenheit)
-            
+            print('Json dumps format : ' + tojson)
             conn.commit()
             conn.close()
             return tojson
-            print('Json dumps format : ' + tojson)
+            
 
 gettemp_()
             #time.sleep(20)
