56812e49487ef71c32c13a1df6571b8b543fbf7f
TomB <automizelife@gmail.com>
2018-05-21 23:55:38 -0700

    reverting back

--- getHistoryModel.py
+++ getHistoryModel.py
@@ -5,19 +5,15 @@ def getHistory():
   conn = sqlite3.connect('weatherdatabase.db')
   c = conn.cursor()
 
-
   c.execute(""" select * from timetemp """)
   data=c.fetchall()
 
   toJson = json.dumps(data)
 
-  split1 = toJson.split(',')
-  print(split1.split(',')) 
-  
   #print(data)
   #return toJson
   return data
 
-getHistory()      #for debugging
+#getHistory()      #for debugging
 
 
