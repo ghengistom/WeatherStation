795b20da4f424670ebea68be2c61f6815d7d0c85
TomB <automizelife@gmail.com>
2018-05-22 01:25:27 -0700

    print one in the list

---
+++ getHistoryModel2.py
@@ -0,0 +1,18 @@
+import sqlite3
+import json
+
+def getHistory():
+  conn = sqlite3.connect('weatherdatabase.db')
+  c = conn.cursor()
+
+  c.execute(""" select time from timetemp """)
+  data=c.fetchall()
+
+  toJson = json.dumps(data)
+
+  print('fetchall : ' + data)
+  print('fetchall jsondumps' + toJson)
+  #return toJson
+  return data
+
+getHistory()
