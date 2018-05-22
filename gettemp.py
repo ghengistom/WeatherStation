50762b45a7ac18426370db97bc87b47dbe09d145
TomB <automizelife@gmail.com>
2018-05-21 20:39:40 -0700

    cleaned up gettemp.py

--- gettemp.py
+++ gettemp.py
@@ -19,14 +19,9 @@ def gettemp_():
         while True:
             conn = sqlite3.connect('weatherdatabase.db')
             c = conn.cursor()
-        #far = sensor.get_temperature()*9/5 + 32 
-        #print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
-        #print(far)
 
             unix = int(time.time())
             date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
-#        keyword = 'Python'
-#        value = random.randrage(0,10)
 
         #commit and close connection for local db
 
@@ -34,10 +29,7 @@ def gettemp_():
             temperature_in_celsius = sensor.get_temperature()
             jsonTest = json.loads(str(temperature_in_celsius))
             temperature_in_fahrenheit = sensor.get_temperature(W1ThermSensor.DEGREES_F)
-        # temperature_in_all_units = sensor.get_temperatures([
-        #     W1ThermSensor.DEGREES_C,
-        #     W1ThermSensor.DEGREES_F,
-        #     W1ThermSensor.KELVIN])    
+      
 
         #insert a row of data into local db
             c.execute('''INSERT INTO timetemp( time, temp )
@@ -54,8 +46,6 @@ def gettemp_():
             conn.close()
             return tojson
             
-
-gettemp_()
-            #time.sleep(20)
+#gettemp_()             #for debugging
             
             
