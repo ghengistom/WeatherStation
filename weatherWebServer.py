5483f919062f463eec97efcdb53e5fef4ed3fe10
TomB <automizelife@gmail.com>
2018-06-02 00:34:28 -0700

    404

--- weatherWebServer.py
+++ weatherWebServer.py
@@ -23,7 +23,7 @@ def get_temp():
 
 @app.route('/gethistorypage', methods=['GET'])
 def get_historypage():
-   return render_template('./gethistory.html', methods=['GET'])
+   return render_template('gethistory.html', methods=['GET'])
 
 
 @app.route('/test1', methods=['GET']) #GET requests will be blocked
