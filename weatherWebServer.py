e288929234b1b4853b18225c7055b7c8c44036df
TomB <automizelife@gmail.com>
2018-06-03 21:06:20 -0700

    jk

--- weatherWebServer.py
+++ weatherWebServer.py
@@ -33,6 +33,6 @@ def test1():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='192.168.1.208', port = 9000, debug = False)
 # if __name__ == '__main__':
 #     app.run(debug=True, port=5000) #run app in debug mode on port 5000
